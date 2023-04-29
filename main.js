a17992f7bc4134751f368d37e2161ba4ccd0f895
Patil <Gaurav.Patil@PerkinElmer.com>
2023-04-29 12:00:11 +0530

    Home section completed

--- main.js
+++ main.js
@@ -34,3 +34,15 @@ function linkAction(){
 
 
 navLink.forEach(n => n.addEventListener('click' , linkAction))
+
+
+// change background header
+
+function scrollHeader() {
+    const nav = document.getElementById('header');
+
+    // when scroll is greater than 100 vh , add scroll header
+    if (this.scrollY >= 100) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll' , scrollHeader);
